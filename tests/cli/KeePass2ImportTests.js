b7eb21b545e425943652639e537c64636eec6a11
Perry Mitchell <perry@perrymitchell.net>
2019-09-24 22:37:37 +0300

    Add kdbx4 archive, update tests (failing)

--- tests/cli/KeePass2ImportTests.js
+++ tests/cli/KeePass2ImportTests.js
@@ -7,118 +7,241 @@ var ButtercupImporter = require("../../dist/buttercup-importer.js"),
     encodingTools = Buttercup.tools.encoding;
 
 module.exports = {
-    setUp: function(cb) {
-        this.exampleArchive = path.resolve(
-            __dirname,
-            "../resources/test-archive.kdbx"
-        );
-        this.examplePassword = "passw0rd";
-        cb();
-    },
-
-    export: {
-        createsArchive: function(test) {
-            ButtercupImporter.importFromKDBX(
-                this.exampleArchive,
-                this.examplePassword
-            ).then(function(archive) {
-                test.ok(
-                    archive instanceof Archive,
-                    "Archive should be a Buttercup archive instance"
-                );
-                test.done();
-            });
+    kdbx3: {
+        setUp: function(cb) {
+            this.exampleArchive = path.resolve(
+                __dirname,
+                "../resources/test-archive-kdbx3.kdbx"
+            );
+            this.examplePassword = "passw0rd";
+            cb();
         },
 
-        containsGroups: function(test) {
-            ButtercupImporter.importFromKDBX(
-                this.exampleArchive,
-                this.examplePassword
-            ).then(function(archive) {
-                var rootGroup = archive.getGroups()[0];
-                test.strictEqual(
-                    rootGroup.getTitle(),
-                    "Testing",
-                    "Root group should be called 'Testing'"
-                );
-                var children = rootGroup.getGroups(),
-                    generalGroup;
-                children.forEach(function(child) {
-                    if (child.getTitle() === "General") {
-                        generalGroup = child;
-                    }
+        export: {
+            createsArchive: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    this.examplePassword
+                ).then(function(archive) {
+                    test.ok(
+                        archive instanceof Archive,
+                        "Archive should be a Buttercup archive instance"
+                    );
+                    test.done();
                 });
-                test.ok(
-                    generalGroup instanceof Group,
-                    "Archive should contain General group"
-                );
-                test.done();
-            });
-        },
+            },
 
-        containsEntry: function(test) {
-            ButtercupImporter.importFromKDBX(
-                this.exampleArchive,
-                this.examplePassword
-            )
-                .then(function(archive) {
-                    var children = archive.getGroups()[0].getGroups(),
+            containsGroups: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    this.examplePassword
+                ).then(function(archive) {
+                    var rootGroup = archive.getGroups()[0];
+                    test.strictEqual(
+                        rootGroup.getTitle(),
+                        "Testing",
+                        "Root group should be called 'Testing'"
+                    );
+                    var children = rootGroup.getGroups(),
                         generalGroup;
                     children.forEach(function(child) {
                         if (child.getTitle() === "General") {
                             generalGroup = child;
                         }
                     });
-                    var sampleEntry = generalGroup.findEntriesByProperty(
-                        "title",
-                        /^Test-entry$/
-                    )[0];
-                    test.strictEqual(
-                        sampleEntry.getProperty("title"),
-                        "Test-entry",
-                        "Title should be correct"
-                    );
-                    test.strictEqual(
-                        sampleEntry.getProperty("username"),
-                        "buttercup",
-                        "Username should be correct"
-                    );
-                    test.strictEqual(
-                        sampleEntry.getProperty("password"),
-                        "westley",
-                        "Password should be correct"
+                    test.ok(
+                        generalGroup instanceof Group,
+                        "Archive should contain General group"
                     );
                     test.done();
-                })
-                .catch(function(err) {
-                    console.error("Failed: " + err.message);
                 });
+            },
+
+            containsEntry: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    this.examplePassword
+                )
+                    .then(function(archive) {
+                        var children = archive.getGroups()[0].getGroups(),
+                            generalGroup;
+                        children.forEach(function(child) {
+                            if (child.getTitle() === "General") {
+                                generalGroup = child;
+                            }
+                        });
+                        var sampleEntry = generalGroup.findEntriesByProperty(
+                            "title",
+                            /^Test-entry$/
+                        )[0];
+                        test.strictEqual(
+                            sampleEntry.getProperty("title"),
+                            "Test-entry",
+                            "Title should be correct"
+                        );
+                        test.strictEqual(
+                            sampleEntry.getProperty("username"),
+                            "buttercup",
+                            "Username should be correct"
+                        );
+                        test.strictEqual(
+                            sampleEntry.getProperty("password"),
+                            "westley",
+                            "Password should be correct"
+                        );
+                        test.done();
+                    })
+                    .catch(function(err) {
+                        console.error("Failed: " + err.message);
+                    });
+            },
+
+            throwsForIncorrectPassword: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    "wrong password"
+                ).then(
+                    function() {
+                        throw new Error(
+                            "Should not be here: error should have thrown"
+                        );
+                    },
+                    function(err) {
+                        test.strictEqual(
+                            err.name,
+                            "KdbxError",
+                            "Error name should match"
+                        );
+                        test.strictEqual(
+                            err.code,
+                            "InvalidKey",
+                            "Error code should match"
+                        );
+                        test.done();
+                    }
+                );
+            }
+        }
+    },
+
+    kdbx4: {
+        setUp: function(cb) {
+            this.exampleArchive = path.resolve(
+                __dirname,
+                "../resources/test-archive-kdbx4.kdbx"
+            );
+            this.examplePassword = "passw0rd";
+            cb();
         },
 
-        throwsForIncorrectPassword: function(test) {
-            ButtercupImporter.importFromKDBX(
-                this.exampleArchive,
-                "wrong password"
-            ).then(
-                function() {
-                    throw new Error(
-                        "Should not be here: error should have thrown"
-                    );
-                },
-                function(err) {
+        export: {
+            createsArchive: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    this.examplePassword
+                )
+                    .then(function(archive) {
+                        test.ok(
+                            archive instanceof Archive,
+                            "Archive should be a Buttercup archive instance"
+                        );
+                        test.done();
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+            },
+
+            containsGroups: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    this.examplePassword
+                ).then(function(archive) {
+                    var rootGroup = archive.getGroups()[0];
                     test.strictEqual(
-                        err.name,
-                        "KdbxError",
-                        "Error name should match"
+                        rootGroup.getTitle(),
+                        "Testing",
+                        "Root group should be called 'Testing'"
                     );
-                    test.strictEqual(
-                        err.code,
-                        "InvalidKey",
-                        "Error code should match"
+                    var children = rootGroup.getGroups(),
+                        generalGroup;
+                    children.forEach(function(child) {
+                        if (child.getTitle() === "General") {
+                            generalGroup = child;
+                        }
+                    });
+                    test.ok(
+                        generalGroup instanceof Group,
+                        "Archive should contain General group"
                     );
                     test.done();
-                }
-            );
+                });
+            },
+
+            containsEntry: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    this.examplePassword
+                )
+                    .then(function(archive) {
+                        var children = archive.getGroups()[0].getGroups(),
+                            generalGroup;
+                        children.forEach(function(child) {
+                            if (child.getTitle() === "General") {
+                                generalGroup = child;
+                            }
+                        });
+                        var sampleEntry = generalGroup.findEntriesByProperty(
+                            "title",
+                            /^Test-entry$/
+                        )[0];
+                        test.strictEqual(
+                            sampleEntry.getProperty("title"),
+                            "Test-entry",
+                            "Title should be correct"
+                        );
+                        test.strictEqual(
+                            sampleEntry.getProperty("username"),
+                            "buttercup",
+                            "Username should be correct"
+                        );
+                        test.strictEqual(
+                            sampleEntry.getProperty("password"),
+                            "westley",
+                            "Password should be correct"
+                        );
+                        test.done();
+                    })
+                    .catch(function(err) {
+                        console.error("Failed: " + err.message);
+                    });
+            },
+
+            throwsForIncorrectPassword: function(test) {
+                ButtercupImporter.importFromKDBX(
+                    this.exampleArchive,
+                    "wrong password"
+                ).then(
+                    function() {
+                        throw new Error(
+                            "Should not be here: error should have thrown"
+                        );
+                    },
+                    function(err) {
+                        test.strictEqual(
+                            err.name,
+                            "KdbxError",
+                            "Error name should match"
+                        );
+                        test.strictEqual(
+                            err.code,
+                            "InvalidKey",
+                            "Error code should match"
+                        );
+                        test.done();
+                    }
+                );
+            }
         }
     }
 };
